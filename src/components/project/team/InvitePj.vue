e65e52a4035070e0d8da6fdb157b8441763fe1c4
YeinYoon <yeinyun23@gmail.com>
2022-03-18 17:16:32 +0900

    프로젝트 유저 초대 컴포넌트 생성

---
+++ src/components/project/team/InvitePj.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+      <select v-model="searchType">
+          <option value="I" selected>아이디</option>
+          <option value="N">닉네임</option>
+      </select>
+      <input type="text" v-model="searchData"> <button @click="userSearch()">유저 검색</button>
+
+      <table>
+          <thead>
+              <th>ID</th>
+              <th>닉네임</th>
+          </thead>
+          <tbody>
+              <tr v-for="user in userList" :key="user.ID">
+                  <td>{{user.ID}}</td>
+                  <td>{{user.NICKNAME}}</td>
+              </tr>
+          </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name : "InvitePj",
+    data() {
+        return {
+            searchData : "",
+            searchType : "",
+            userList : [],
+        }
+    },
+    methods : {
+        userSearch() {
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
