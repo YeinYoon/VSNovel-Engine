a0c6b98e73c4335eca0094e37976c331195bb57f
YeinYoon <yeinyun23@gmail.com>
2022-04-02 17:30:44 +0900

    엔진 빌드테스트 성공

--- src/background.js
+++ src/background.js
@@ -35,7 +35,7 @@ async function createWindow() {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadURL('http://localhost:8080/')
+    win.loadURL('http://52.79.121.208:2022/')
   }
 }
 
