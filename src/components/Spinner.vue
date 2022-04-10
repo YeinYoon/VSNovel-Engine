e799fb1a9a770296dd3aeaa21a849502bf249d1b
GitSeokbeom <93910030+GitSeokbeom@users.noreply.github.com>
2022-04-10 22:58:53 +0900

    스피너 개선

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,61 +1,66 @@
 <template>
-    <div class="lds-facebook" v-if="loading">
-        <div>
-        </div>
-        <div>
-        </div>
-        <div>
-        </div>
-    </div>
+  <div class="lds-facebook" v-if="loading">
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 </template>
 <script>
 export default {
-    props: {
-        loading: {
-            type: Boolean,
-            required: true
-        }
-    }
-}
+  props: {
+    loading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
 </script>
 <style>
 .lds-facebook {
-    z-index: 1;
-    display: inline-block;
-    position: absolute;
-    width: 64px;
-    height: 64px;
-    top: 47%;
-    left: 47%
+  display: inline-block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
 }
 .lds-facebook div {
-    display: inline-block;
-    position: absolute;
-    left: 6px;
-    width: 13px;
-    background: #4845e7;
-    animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+  display: inline-block;
+  position: absolute;
+  left: 6px;
+  width: 15px;
+  margin-left: 10px;
+  border-radius: 250px;
+  background: #2872f9;
+  box-shadow: 0px 0px 10px 2px rgba(24, 53, 113, 0.7);
+  animation: lds-facebook 1.3s cubic-bezier(0, 0.5, 0.5, 1) infinite;
 }
 .lds-facebook div:nth-child(1) {
-    left: 6px;
-    animation-delay: -0.24s;
+  left: 0px;
+  animation-delay: -0.24s;
 }
 .lds-facebook div:nth-child(2) {
-    left: 26px;
-    animation-delay: -0.12s;
+  left: 30px;
+  animation-delay: -0.12s;
 }
 .lds-facebook div:nth-child(3) {
-    left: 45px;
-    animation-delay: 0;
+  left: 60px;
+  animation-delay: 0s;
 }
 @keyframes lds-facebook {
-    0% {
-        top: 6px;
-        height: 51px;
-    }
-    50%, 100% {
-        top: 19px;
-        height: 25px;
-    }
+  0% {
+    opacity: 0.5;
+    top: -50px;
+    transform: scale(0.1);
+    height: 10px;
+  }
+  50%,
+  100% {
+    top: 0px;
+    transform: scale(1);
+    height: 15px;
+  }
 }
 </style>
