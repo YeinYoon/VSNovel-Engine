30306720a422e5698998ef9e57fc0d6cd15dad39
JungIn <lego0520lwy@icloud.com>
2022-05-09 15:43:05 +0900

    merge

--- src/main.js
+++ src/main.js
@@ -3,9 +3,8 @@ import App from './App.vue'
 import Router from './router.js'
 import Store from './store.js'
 import VueQuillEditor from 'vue-quill-editor'
-import VueHtml2Canvas from 'vue-html2canvas'
 
 
 let app = createApp(App);
 
-app.use(Router).use(Store).use(VueQuillEditor).use(VueHtml2Canvas).mount('#app')
+app.use(Router).use(Store).use(VueQuillEditor).mount('#app')
