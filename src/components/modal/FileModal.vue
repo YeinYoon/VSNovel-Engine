354563865f211252967f0bc9f19e66646790b160
GitSeokbeom <93910030+GitSeokbeom@users.noreply.github.com>
2022-05-17 11:45:03 +0900

    파일선택 지움

--- src/components/modal/FileModal.vue
+++ src/components/modal/FileModal.vue
@@ -142,6 +142,10 @@ export default {
   /* margin-bottom: 10px; */
 }
 
+.Fmodal_UploadAreaInput::file-selector-button {
+  display: none;
+}
+
 .Fmodal_UploadArea:hover{
   opacity: 0.4;
 }
@@ -174,10 +178,12 @@ export default {
 
 .Fmodal_UploadArea:hover .UploadBefore {
   opacity: 0;
+  display: none;
 }
 
 .Fmodal_UploadArea:hover .UploadAfter {
   opacity: 1;
+  display: block;
 }
 
 .Fmodal_UploadAreaInput:hover .UploadBefore {
