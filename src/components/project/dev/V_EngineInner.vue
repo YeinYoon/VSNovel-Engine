45ad41e00af472a44785f11772c09c1f69b2ab9a
GitSeokbeom <93910030+GitSeokbeom@users.noreply.github.com>
2022-05-24 09:17:30 +0900

    Update V_EngineInner.vue

--- src/components/project/dev/V_EngineInner.vue
+++ src/components/project/dev/V_EngineInner.vue
@@ -36,38 +36,90 @@
         </div> -->
 
         <div class="VpcTopToolbar"> <!-- 플롯 추가등의 버튼 -->
-          <div class="VpcTool_addplot">
-            <p>플롯추가</p>
+          <div class="VpcToolPosition">
+            <button>플롯추가</button>
+            <button>엔딩추가</button>
           </div>
+
         </div> <!-- 플롯 추가등의 버튼 -->
 
-        <div class="VpcInner"> <!-- 플롯 컨트롤러 상단 도구와 분리 -->     
+        <div class="VpcInner"> 
           
+          <!-- 플롯리스트 -->
+          <!-- 플롯의 갯수만큼 반복 -->
           <div class="VpcBlock"> <!-- 플롯 박스 -->
             <div class="VpcBlockLabel"> <!-- 플롯 라벨 및 열기버튼 -->
               <div class="VpcBlock_Title"><p>플롯제목제목</p></div>
               <div class="VpcBlock_Opener"><p>ㅜ</p></div>
             </div>
 
-            <div class="VpcBlockInner"> <!-- 플롯 내부 조회 -->
+            <!-- 플롯 내부 조회 --> 
+            <!-- 이 플롯이 가진 페이지만큼 반복 -->
+            <!-- 반복문 실행중 일반 페이지는 VpcPageNormal 클래스를 붙일것. -->
+            <!-- 반복문 실행중 선택자 페이지는 VpcPageSelect 클래스를 붙일것. -->
+            <div class="VpcBlockInner"> 
+              
+              <!-- 일반 페이지 -->
+              <!-- 일반 페이지는 단순 대화를 담고있음. -->
+              <!-- 일반 페이지는 다음 페이지로의 이동만 함. (플롯간 이동X) -->
               <div class="VpcPageNormal">
                 123123
               </div>
 
-              <div class="VpcPageSelect">
-                12124214
-              </div>
-
               <div class="VpcPageNormal">
                 123123
               </div>
 
+              <div class="VpcPageNormal">
+                112313213213
+              </div>
+
+              <!-- 선택자 페이지 -->
+              <!-- 플레이어가 선택하면, 다른 플롯으로의 이동이 발생함 -->
+              <!-- 페이지에 선택지를 추가한 갯수만큼 반복문을 돌릴것. -->
               <div class="VpcPageSelect">
-                12124214
-              </div>  
+                <div class="VpcPageSel">
+                  <div class="VpcPageSel">선택지1</div>
+                  <div class="VpcPageSelOrigin"> <!-- 선택지이름 -->
+
+                  </div>
+                  <div class="VpcPageSelChange"> <!-- 선택 이후의 플롯인덱스 -->
+
+                  </div>
+                </div>
+
+              </div>
+
+              <div class="VpcBlockControl">
+                <button>선택지 추가</button>
+                <button>페이지 추가</button>
+              </div>
             </div><!-- 플롯 블록 이너 끝 -->
 
           </div> <!-- 플롯 블록 끝 -->
+
+
+          <!-- 엔딩플롯 예시 -->
+          <!-- 엔딩플롯이란, 유저가 선택한 선택지를 통해 이동된 플롯중 하나임. -->
+          <!-- 엔딩플롯의 마지막 페이지에서는 비주얼노벨 종료 명령이 전달되도록 한다. -->
+          <!-- 즉 웹툰에서 마지막 페이지를 담당하는것임. -->
+          <!-- 플롯 반복문에서 가장 마지막에 반복되도록 만든다 -->
+          <div class="VpcBlock"> <!-- 플롯 박스 -->
+            <div class="VpcBlockLabel_end"> <!-- 플롯 라벨 및 열기버튼 -->
+              <div class="VpcBlock_Title"><p>엔딩1 - 123</p></div>
+              <div class="VpcBlock_Opener"><p>ㅜ</p></div>
+            </div>
+            <div class="VpcBlockInner">
+              <div class="VpcPageNormal">
+                엄준식
+              </div>
+
+              <div class="VpcPageNormal">
+                엄 어엄 엄
+              </div>
+
+            </div>
+          </div>
 
 
         </div> <!-- VpcInner 끝 -->
@@ -173,22 +225,34 @@ export default {
 }
 
 .VpcTopToolbar {
+  position: relative;
   width: 100%;
   height: 50px;
   border-radius: 20px;
   background: rgb(88, 88, 88);
+  padding: 10px;
 }
 
-.VpcTool_addplot {
-  width: 60px;
-  height: 30px;
+.VpcToolPosition {
+  width: 100%;
   position: relative;
-  left: 10px;
-  top: 10px;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+
+}
+
+.VpcToolPosition button{
   background: #2872f9;
   border-radius: 10px;
-  display: table;
+  font-size: 0.9em;
+  color: white;
+  display: inline-block;
   cursor: pointer;
+  border: none;
+  padding: 5px;
+  margin: 2px;
 }
 
 .VpcTool_addplot p {
@@ -214,6 +278,13 @@ export default {
   width: 100%;
   height: 40px;
   background: rgb(71, 71, 71);
+  border-radius: 10px;
+}
+
+.VpcBlockLabel_end {
+  width: 100%;
+  height: 40px;
+  background: #0b3c99;
   border-radius: 10px;
 }
 
@@ -256,10 +327,15 @@ export default {
 
 .VpcPageNormal {
   width: 100%;
-  background: rgb(161, 161, 161);
+  background: #949494;
   border-radius: 5px;
   padding: 5px;
   margin-bottom: 5px;
+}
+
+.VpcPageNormal:hover {
+  background: #858585;
+  cursor: pointer;
 }
 
 .VpcPageSelect {
@@ -270,4 +346,23 @@ export default {
   margin-bottom: 5px;
 }
 
+.VpcBlockControl {
+  width: 100%;
+}
+
+.VpcBlockControl button {
+  position: relative;
+  left: 34px;
+  background: #2872f9;
+  border: none;
+  border-radius: 10px;
+  color: white;
+  font-size: 12px;
+  padding: 5px;
+  margin: 1px;
+}
+
+.VpcBlockControl button:hover {
+  background: #0084ff;
+}
 </style>
