8456811bdc829c1936d5525c4ab0c48d3d1787fa
JungIn <lego0520lwy@icloud.com>
2022-06-22 10:02:20 +0900

    생성오류 완료

--- src/components/project/createPj/InputPj.vue
+++ src/components/project/createPj/InputPj.vue
@@ -43,7 +43,8 @@ export default {
       .then(async (result)=>{
         if(result.data!="err") {
           var data = JSON.stringify({ // 프로젝트 기본 구조
-            "scenario":{}
+            "id":"","title":"","ep":"",
+            "plotList":[{"plotName":"시작 플롯","nextPlot":0,"pages":[{"pageName":"일반 페이지","type":"n","bg":"","bgm":"","effect":"","name":"이름","text":"대화 및 내용","img":"","select":[{"use":true,"text":"1번 선택지","plot":0,"index":0},{"use":true,"text":"2번 선택지","plot":0,"index":0},{"use":true,"text":"3번 선택지","plot":0,"index":0}]}]}]
           });
           var fileName = `PJ${result.data.pjCode}.json`
           var properties = {type:'text/plain'};
