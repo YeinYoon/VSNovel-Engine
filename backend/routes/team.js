17069ea4a58c6058779277e24ecc99df26b88d7a
JungIn <lego0520lwy@icloud.com>
2022-07-06 20:53:01 +0900

    회원 지분, 일정 정리중

--- backend/routes/team.js
+++ backend/routes/team.js
@@ -94,26 +94,30 @@ router.post('/PjRefuse', async (req,res)=>{
 
 //현 프로젝트에 참여중인 멤버 목록
 router.post('/memberList', async (req, res)=>{
-    var memberList = await db.execute(`SELECT user_id, '' as user_nickname, proj_code, coop_stake FROM tbl_cooperation
+    var memberList = await db.execute(`SELECT user_id, (SELECT user_nickname FROM tbl_user WHERE tbl_user.user_id = tbl_cooperation.user_id) as user_nickname, proj_code, coop_stake FROM tbl_cooperation
     WHERE proj_code=${req.body.pjCode}`);
     if(memberList == "err") {
         res.send("err");
     } else {
-        var list = [];
+        let list = [];
         await memberList.rows.forEach(async (item)=>{
-            let temp =await db.execute(`SELECT user_nickname FROM tbl_user WHERE user_id = '${item.USER_ID}'`)
-            item.USER_NICKNAME = temp.rows[0].USER_NICKNAME
-            console.log(item)
-            // if(item.USER_ID == `${req.user.USER_ID}`) {
-            //     list.unshift(item);
-            // } else {
+            if(item.USER_ID == `${req.user.USER_ID}`) {
+                list.unshift(item);
+            } else {
                 list.push(item)
-            // }
-            console.log(list)
+            }
         })
-        console.log(list)
         res.send(list);
     }
 })
 
+router.post('/uploadMemberList', async (req, res)=>{
+    for(let i=0;i<req.body.list.length;i++){
+        var update = await db.execute(`UPDATE tbl_cooperation SET coop_stake = ${req.body.list[i].COOP_STAKE} WHERE user_id = '${req.body.list[i].USER_ID}' AND proj_code = '${req.body.list[i].PROJ_CODE}'`)
+        if(update == "err"){
+            res.send("err")
+        }
+    }
+    res.send("ok")
+})
 module.exports = router;
